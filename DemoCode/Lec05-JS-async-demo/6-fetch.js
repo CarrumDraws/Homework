c2c33d3393fff6d5613fa82d5c426ee417ffa35d
Carrum <calumchansy@gmail.com>
2024-05-29 16:57:19 -0700

    Halfway done with homework

---
+++ DemoCode/Lec05-JS-async-demo/6-fetch.js
@@ -0,0 +1,77 @@
+// ------------------------------------------------------------------------------------
+// // What does a fetch response look like?
+// const fetchResult = fetch("https://jsonplaceholder.typicode.com/todos/1");
+// console.log("fetchResult =", fetchResult); // Promise { <pending> } : fetchResult isn't resolved yet
+
+// fetchResult.then((response) => {
+//   console.log("response =", response); // Response {object} : The response to your request. Contains info like headers, status code, and body.
+//   console.log("response.json() =", response.json()); // Promise { <pending> } : response.json() reads response stream + returns a promise that parses the response body. (This is an async operation)
+// });
+
+// // How do we actually get the data?
+// fetchResult
+//   .then((response) => response.json())
+//   .then((data) => console.log(data)) // Wait for pesponse.json() to resolve to get the actual data!
+//   .catch((error) => console.log(error));
+
+// -----------------------------------------
+
+// // Handling response errors by looking at response HTTP status code.
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((response) => {
+//     console.log("response =", response);
+//     if (response.ok) return response.json();
+//     else throw new Error(`Error: Status ${response.status}`);
+//     // no explict return, return undefined
+//     // non-promise values => promise. Promise.resolve(undefined)
+//   })
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
+
+// ------------------------------------------------------------------------------------
+// When button is clicked, make an API request and display the response title on the webpage.
+// Get data from https://jsonplaceholder.typicode.com
+
+// const btn = document.getElementById("fetchBtn");
+// btn.addEventListener("click", function () {
+//   fetch("https://jsonplaceholder.typicode.com/todos/1")
+//     .then((response) => {
+//       if (response.ok) return response.json();
+//       else throw new Error(`Error: Status ${response.status}`);
+//     })
+//     .then((data) => {
+//       const fetchP = document.getElementById("fetchP");
+//       fetchP.innerText = `Title: ${data.title}`;
+//     })
+//     .catch((error) => console.log(error));
+// });
+
+// ------------------------------------------------------------------------------------
+// Fetch POST request
+
+// const btn = document.getElementById('fetchBtn');
+// btn.addEventListener('click', function () {
+//   const update = {
+//     title: 'Sample Title',
+//     body: 'sample body',
+//     userId: 1,
+//   };
+
+//   fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(update),
+//   })
+//     .then(response => {
+//       if (response.ok) return response.json();
+//       else throw new Error(response.status);
+//     })
+//     .then(data => {
+//       console.log(data);
+//     })
+//     .catch(e => {
+//       console.log(e);
+//     });
+// });
